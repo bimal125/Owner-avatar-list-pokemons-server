6b592da9aa2d265c1e00c69393a604c270aded31
bimal125 <pandeybi125@gmail.com>
2023-12-12 21:38:35 +0545

    Add cors settings

--- main.py
+++ main.py
@@ -1,10 +1,48 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi_pagination import add_pagination
 
 from config.database import init_db
 from apps.pokemon.pokemon_routers import router as pokemon_routers
 
 app = FastAPI()
+
+ORIGINS = [
+    # Localhost
+    "http://localhost:3080",
+    "http://127.0.0.1:3080",
+]
+
+CORS_ALLOW_METHODS = (
+    'DELETE',
+    'GET',
+    'OPTIONS',
+    'PATCH',
+    'POST',
+    'PUT',
+)
+
+CORS_ALLOW_HEADERS = (
+    'accept',
+    'accept-encoding',
+    'authorization',
+    'content-type',
+    'dnt',
+    'origin',
+    'user-agent',
+    'x-csrftoken',
+    'x-requested-with',
+    'sentry-trace',
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=ORIGINS,
+    allow_credentials=True,
+    allow_methods=CORS_ALLOW_METHODS,
+    allow_headers=CORS_ALLOW_HEADERS,
+)
+
 
 @app.on_event("startup")
 async def on_startup():
